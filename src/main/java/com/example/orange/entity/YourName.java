db9af8e7ab8f140d3fcdf9d84165c8f43134c116
Dream Yang <l>
2019-03-24 15:17:57 +0800

    创建一个实体类User提交

---
+++ src/main/java/com/example/orange/entity/YourName.java
@@ -0,0 +1,19 @@
+package com.example.orange.entity;
+
+public class YourName {
+    private String name;
+
+    public YourName(){}
+    public YourName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
